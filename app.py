9e49e6202299092acce73aff0ce99634e844a3dc
saydaxmatov <saydaxmatovrustam@gmail.com>
2025-09-01 12:58:08 +0000

    kutubxonalarni oldim

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 # O‘qitilgan modelni yuklash
 model = YOLO("runs/detect/train/weights/best.pt")
 
-# Sinov
+# Sinov rasm ko'rsamiz
 results = model.predict("pomidor.jpg", conf=0.5, iou=0.6)
 results[0].show()
 
